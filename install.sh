4958b0c0bf22461c8f18fe2ec9a91b6e3d618d1b
Ali Omran <aliomran93@gmail.com>
2022-08-17 13:20:34 +0200

    Add X11 under linux in install

--- install.sh
+++ install.sh
@@ -25,11 +25,7 @@ mkdir -p "$XDG_CONFIG_HOME/nvim/undo"
 ln -sf "$DOTFILES/nvim/init.vim" "$XDG_CONFIG_HOME/nvim"
 ln -sf "$DOTFILES/nvim/lua" "$XDG_CONFIG_HOME/nvim"
 
-rm -rf "$XDG_CONFIG_HOME/X11"
-ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
-
 # Install plugin manager
-
 [ ! -f "$DOTFILES/nvim/autoload/plug.vim" ] \
     && curl -fLo "$DOTFILES/nvim/autoload/plug.vim" --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -61,6 +57,13 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
     mkdir -p "$XDG_CONFIG_HOME/dunst"
     ln -sf "$DOTFILES/dunst/dunstrc" "$XDG_CONFIG_HOME/dunst/dunstrc"
+
+    #######
+    # X11 #
+    #######
+
+    rm -rf "$XDG_CONFIG_HOME/X11"
+    ln -s "$DOTFILES/X11" "$XDG_CONFIG_HOME"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     ####################
     # Alacritty Mac OS # 
